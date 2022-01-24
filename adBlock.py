e8a3ce61a7169d58c45f6d9192950d2009339964
Jtility37 <leejewon97@gmail.com>
2022-01-25 03:48:26 +0900

    pyinstaller -F --add-data "chromedriver.exe;." --add-data "test.txt;." adBlock.py 로 해도 -F 없을 때는 되나, 있을 땐 txt마저 안 되는거 확인ㅜ

--- adBlock.py
+++ adBlock.py
@@ -1,13 +1,17 @@
 # x-button : ytp-ad-overlay-close-button
 # skip-button : ytp-ad-skip-button ytp-button
+
 from selenium import webdriver
 import time
 
 try:
-        
-    driver = webdriver.Chrome() # dist\python 파일명 에 chromedriver를 비롯한 필요파일
-    driver.get("https://www.naver.com")
+    # driver = webdriver.Chrome() # dist에 chromedriver를 비롯한 필요파일
+    # driver.get("https://www.naver.com")
+    f = open("test.txt", 'r')
+    print(f.read())
+    f.close()
 except Exception as e:
     print(e)
 print('test')
+
 time.sleep(3)
